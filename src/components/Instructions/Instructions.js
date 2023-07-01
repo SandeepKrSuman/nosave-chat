e275856204a95603e6bad382c653f6fdff3a9941
Sandeep Suman <sandeepkrsuman2409@gmail.com>
2023-07-01 14:54:57 +0530

    modified instruction

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -8,7 +8,7 @@ const Instructions = () => {
       <h3>How to Use?</h3>
       <ol>
         <li>Enter the phone number you want to chat with.</li>
-        <li>Type your message in the input field at the bottom.</li>
+        <li>Type your message in the text box at the bottom.</li>
         <li>
           Click the "Chat on WhatsApp" button to redirect to WhatsApp and start
           the chat.
